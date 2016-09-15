91588ebd87dec894d1715a7abe921aa5085e9bf2
jlkho <jlkho@192.168.56.1>
2016-09-15 08:14:51 +0800

    Adds first regex

--- Regexmatch/src/regexmatch/Regexmatch.java
+++ Regexmatch/src/regexmatch/Regexmatch.java
@@ -3,8 +3,11 @@ package regexmatch;
 
 public class Regexmatch {
 
+    public static boolean isTrue(String str){
+        return str.matches("[tT]rue");
+    }
     public static void main(String[] args) {
-        
+        System.out.println(isTrue("True"));
     }
     
 }
